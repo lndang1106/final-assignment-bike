f3d555b22bba1b9205b4b1ecdf6dcd8897f06cf3
Le Nguyen Dang <lndang1106@gmail.com>
2023-12-18 17:33:40 +0200

    Trying to deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/final-assignment-bike/' : '',
-  trailingSlash: true
 }
 
 module.exports = nextConfig
